f6e89f12f475942800a39ec3b07fdcb504a2a374
RajarshiBarman <rajarshibarman812@gmail.com>
2023-10-16 21:33:43 +0530

    Updated

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def result():
         res = result.to_dict(flat=True)
         print("res:", res)
         arr1 = res.values()
-        arr = ([value for value in arr1])
+        arr = ([int(value) for value in arr1])
 
         data = np.array(arr)
 
@@ -52,51 +52,7 @@ def result():
 
         print(predictions)
 
-        '''
-         pred = loaded_model.predict_proba(data)
-        print(pred)
-        # acc=accuracy_score(pred,)
-        pred = pred > 0.05
-        # print(predictions)
-        i = 0
-        j = 0
-        index = 0
-        res = {}
-        final_res = {}
-        while j < 3:
-            if pred[i, j]:
-                res[index] = j
-                index += 1
-            j += 1
-        # print(j)
-        # print(res)
-        index = 0
-        for key, values in res.items():
-            if values != predictions[0]:
-                final_res[index] = values
-                print('final_res[index]:', final_res[index])
-                index += 1
-        # print(final_res)
-        jobs_dict = {0: 'Science',
-                     1: 'Commerce',
-                     2: 'Arts',
-                     }
-
-        # print(jobs_dict[predictions[0]])
-        job = {}
-        # job[0] = jobs_dict[predictions[0]]
-        index = 1
-
-        data1 = predictions[0]
-        print(data1)
-         if predictions == 1:
-            final='Science'
-        if predictions[0][1] == 1:
-            final='Commerce'
-        if predictions[0][2] == 1:
-            final='Arts'
-        '''
-       
+        
 
         return render_template("results.html",final=predictions[0])
 
